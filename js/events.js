87ef7ab1795c14c34a95c0a8744167299a97e9b3
Nekro-Noire <nekro_cannibal_violahuesos@hotmail.com>
2016-09-10 17:38:34 -0500

    :nail_care:
    :poop:

--- js/events.js
+++ js/events.js
@@ -1,66 +1,193 @@
+var consulta = window.matchMedia('(max-width: 800px)');
+consulta.addListener(mediaQuery);
+
+var $burguerButton = document.getElementById('burguer-button');
+
+
+function mediaQuery() {
+    if (consulta.matches) {
+        ///////////////////////////////////////PANTALLAS DE MENOS DE 800PX//////////////////////////////////////////////
+        $(".negro").click(function () {
+            $(".verde").animate({
+                "height": "0px"
+            }, "slow");
+            $(".azul").animate({
+                "height": "0px"
+            }, "slow");
+            $(this).animate({
+                "height": "600px"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+            $(".mundo1").css('display', 'block');
+            $("#otracosa").css('display', 'none');
+
+            console.log('asd');
+            particlesJS('particles-js', jsonPuntos);
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = 'body {}';
+            document.getElementsByTagName('head')[0].appendChild(style);
+            this.stylesheet = document.styleSheets[document.styleSheets.length - 1];
+            try {
+                //  console.log('@-webkit-keyframes walkW{from {top: '+$('.lunaP').position().top+'px; left:'+$('.lunaP').position().left+'px}}');
+                this.stylesheet.insertRule('@-webkit-keyframes walkW{to {top: ' + $('.lunaP').position().top + 'px; left:' + $('.lunaP').position().left + 'px},from {top: 1000px; left:10000px}}', this.stylesheet.rules.length);
+                //  this.stylesheet.insertRule('.objetoN{         -webkit-animation: walkW '+velocity+'.0s linear  infinite;}', this.stylesheet.rules.length);
+            } catch (e) {};
+            velocity -= 5;
+        });
+        
+        $(".verde").click(function () {
+            $(".negro").animate({
+                "height": "0px"
+            }, "slow");
+            $(".azul").animate({
+                "height": "0px"
+            }, "slow");
+            $(this).animate({
+                "height": "600px"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+        });
+        
+        $(".azul").click(function () {
+            $(".negro").animate({
+                "height": "0px"
+            }, "slow");
+            $(".verde").animate({
+                "height": "0px"
+            }, "slow");
+            $(this).animate({
+                "height": "600px"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+        });
+
+        $(".pestana").click(function () {
+            $(".pestana").hide();
+            $(".negro").animate({
+                "height": "200px"
+            }, "slow");
+            $(".verde").animate({
+                "height": "200px"
+            }, "slow");
+            $(".azul").animate({
+                "height": "200px"
+            }, "slow");
+
+            $(".mundo1").css('display', 'none');
+            $("#otracosa").css('display', 'block');
+            console.log('asd');
+
+        });
+        ///////////////////////////////////////PANTALLAS DE MENOS DE 800PX//////////////////////////////////////////////
+
+    } else {
+
+        ///////////////////////////////////////PANTALLAS DE MÁS DE 800PX//////////////////////////////////////////////
+        $(".negro").click(function () {
+
+            $(".verde").animate({
+                "width": "0%",
+                "left": "100%"
+            }, "slow");
+            $(".azul").animate({
+                "width": "0%",
+                "left": "100%"
+            }, "slow");
+            $(this).animate({
+                "width": "100%",
+                "left": "0%"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+            $(".mundo1").css('display', 'block');
+            $("#otracosa").css('display', 'none');
+
+            console.log('asd');
+            particlesJS('particles-js', jsonPuntos);
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = 'body {}';
+            document.getElementsByTagName('head')[0].appendChild(style);
+            this.stylesheet = document.styleSheets[document.styleSheets.length - 1];
+            try {
+                //  console.log('@-webkit-keyframes walkW{from {top: '+$('.lunaP').position().top+'px; left:'+$('.lunaP').position().left+'px}}');
+                this.stylesheet.insertRule('@-webkit-keyframes walkW{to {top: ' + $('.lunaP').position().top + 'px; left:' + $('.lunaP').position().left + 'px},from {top: 1000px; left:10000px}}', this.stylesheet.rules.length);
+                //  this.stylesheet.insertRule('.objetoN{         -webkit-animation: walkW '+velocity+'.0s linear  infinite;}', this.stylesheet.rules.length);
+            } catch (e) {};
+            velocity -= 5;
+
+
+        });
+
+        $(".verde").click(function () {
+            $(".negro").animate({
+                "width": "0%",
+                "left": "0%"
+            }, "slow");
+            $(".azul").animate({
+                "width": "0%",
+                "left": "100%"
+            }, "slow");
+            $(this).animate({
+                "width": "100%",
+                "left": "0%"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+            console.log('asd');
+
+        });
+
+        $(".azul").click(function () {
+            $(".verde").animate({
+                "width": "0%",
+                "left": "0%"
+            }, "slow");
+            $(".negro").animate({
+                "width": "0%",
+                "left": "0"
+            }, "slow");
+            $(this).animate({
+                "width": "100%",
+                "left": "0%"
+            }, "slow");
+            $(".pestana").fadeIn(1500);
+            console.log('asd');
+
+        });
+        var velocity = 23;
+        $(document).ready(function () {
+            particlesJS('otracosa', jsonPuntos);
+
+
+        });
+        $(".pestana").click(function () {
+            $(".pestana").hide();
+            $(".negro").animate({
+                "width": "33.3%",
+                "left": "0%"
+            }, "slow");
+            $(".verde").animate({
+                "width": "33.3%",
+                "left": "33.3%"
+            }, "slow");
+            $(".azul").animate({
+                "width": "33.3%",
+                "left": "66.6%"
+            }, "slow");
+
+            $(".mundo1").css('display', 'none');
+            $("#otracosa").css('display', 'block');
+            console.log('asd');
+
+        });
+        ///////////////////////////////////////PANTALLAS DE MÁS DE 800PX//////////////////////////////////////////////
+
+    }
+}
+mediaQuery();
 
 
 
-$( ".negro" ).click(function() {
-
-        $( ".verde" ).animate({"width" : "0%", "left" : "100%"}, "slow");
-        $( ".azul" ).animate({"width" : "0%", "left" : "100%"}, "slow");
-        $( this ).animate({"width" : "100%", "left" : "0%"}, "slow");  
-        $( ".pestana").fadeIn(1500);
-			$(".mundo1").css('display','block');
-			$("#otracosa").css('display','none');
-
-      console.log('asd');
-			particlesJS('particles-js',jsonPuntos);
-      var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = 'body {}';
-    document.getElementsByTagName('head')[0].appendChild(style);
-    this.stylesheet = document.styleSheets[document.styleSheets.length-1];
-    try {
-    //  console.log('@-webkit-keyframes walkW{from {top: '+$('.lunaP').position().top+'px; left:'+$('.lunaP').position().left+'px}}');
-      this.stylesheet.insertRule('@-webkit-keyframes walkW{to {top: '+$('.lunaP').position().top+'px; left:'+$('.lunaP').position().left+'px},from {top: 1000px; left:10000px}}', this.stylesheet.rules.length);
-    //  this.stylesheet.insertRule('.objetoN{         -webkit-animation: walkW '+velocity+'.0s linear  infinite;}', this.stylesheet.rules.length);
-        } catch (e) {};
-      velocity-=5;
-
-
-});
-
-$( ".verde" ).click(function() {
-        $( ".negro" ).animate({"width" : "0%", "left" : "0%"}, "slow");
-        $( ".azul" ).animate({"width" : "0%", "left" : "100%"}, "slow");
-        $( this ).animate({"width" : "100%", "left" : "0%"}, "slow");
-        $( ".pestana").fadeIn(1500);
-        console.log('asd');
-
-});
-
-$( ".azul" ).click(function() {
-        $( ".verde" ).animate({"width" : "0%", "left" : "0%"}, "slow");
-        $( ".negro" ).animate({"width" : "0%", "left" : "0"}, "slow");
-        $( this ).animate({"width" : "100%", "left" : "0%"}, "slow");
-        $( ".pestana").fadeIn(1500);
-        console.log('asd');
-
-});
-var velocity=23;
-$(document).ready(function(){
-  particlesJS('otracosa',jsonPuntos);
-
-
-});
-$( ".pestana" ).click(function() {
-        $( ".pestana" ).hide();
-        $( ".negro" ).animate({"width" : "33.3%", "left" : "0%"}, "slow")
-        $( ".verde" ).animate({"width" : "33.3%", "left" : "33.3%"}, "slow")
-        $( ".azul" ).animate({"width" : "33.3%", "left" : "66.6%"}, "slow")
-
-			$(".mundo1").css('display','none');
-      $("#otracosa").css('display','block');
-      console.log('asd');
-
-});
 
 ///////////////////////FUNCION PARA LAS HORAS DEL DIA///////////////////////////////////
 
@@ -68,39 +195,21 @@ var fecha = new Date();
 var hora = fecha.getHours();
 
 if (hora > 20 || hora <= 6) {
-    $( ".negro" ).css('background', '#597e81');
-    $( ".verde" ).css('background', '#de516a');
-    $( ".azul" ).css('background', '#6f4aa0');
+    $(".negro").css('background', '#597e81');
+    $(".verde").css('background', '#de516a');
+    $(".azul").css('background', '#6f4aa0');
 }
 
 if (hora > 6 && hora <= 15) {
-    $( ".negro" ).css('background', '#de516a');
-    $( ".verde" ).css('background', '#6f4aa0');
-    $( ".azul" ).css('background', '#597e81');
+    $(".negro").css('background', '#de516a');
+    $(".verde").css('background', '#6f4aa0');
+    $(".azul").css('background', '#597e81');
 }
 
 if (hora > 15 && hora <= 20) {
-    $( ".negro" ).css('background', '#6f4aa0');
-    $( ".verde" ).css('background', '#597e81');
-    $( ".azul" ).css('background', '#de516a');
+    $(".negro").css('background', '#6f4aa0');
+    $(".verde").css('background', '#597e81');
+    $(".azul").css('background', '#de516a');
 }
 
 ///////////////////////TERMINA  FUNCION PARA LAS HORAS DEL DIA///////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
